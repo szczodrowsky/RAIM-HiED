534d5ae690d3fbc7e818ab910dcf3f19e67ebb6c
szczodrowsky <116168292+szczodrowsky@users.noreply.github.com>
2023-04-03 20:09:22 +0000

    dużo

---
+++ database.py
@@ -0,0 +1,24 @@
+import sqlalchemy
+from sqlalchemy import create_engine, text
+import os
+
+
+db_connection_string = os.environ['DB_CONECTION_STRING']
+
+
+
+
+engine = create_engine(
+  db_connection_string,
+  connect_args={
+    "ssl":{
+      "ssl_ca": "/etc/ssl/cert.pem"
+    }
+  }
+ )
+
+
+with engine.connect() as conn:
+  result = conn.execute(text("select * from koncentracja"))
+  print(result.all())
+  
