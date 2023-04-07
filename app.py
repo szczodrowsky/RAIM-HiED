99d8e73052cdeac824746122d7213671dd0366ea
szczodrowsky <116168292+szczodrowsky@users.noreply.github.com>
2023-04-07 15:40:44 +0000

    dodane testy

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
+import random
 
 app = Flask(__name__)
 
@@ -6,17 +7,54 @@ app = Flask(__name__)
 def page0():
   return render_template('home.html')
 
-@app.route("/1")
+@app.route("/ankieta")
 def page1():
-  return render_template('page1.html')
+  return render_template('ankieta.html')
 
-@app.route("/2")
+@app.route("/opis1")
 def page2():
-  return render_template('page2.html')
+  return render_template('opis1.html')
 
-@app.route("/3")
-def page3():
-  return render_template('page3.html')
+@app.route('/colors')
+def page4():
+  return render_template('colors.html')
+
+@app.route("/opis2")
+def page5():
+  return render_template('opis2.html')
+
+@app.route('/get_color')
+def get_color():
+  colors = ['red', 'blue', 'green']
+  color = random.choice(colors)
+  return jsonify(color)
+
+@app.route('/get_data')
+def get_data():
+  sizes = [125,150,100]
+  numbers = random.sample(range(1, 9), 2)
+  bigger_circle = random.choice([0, 1])
+  if numbers[0] > numbers[1]:
+    command = 'Kliknij większe koło'
+  else:
+    command = 'Wybierz większą cyfrę'
+  random_sizes = random.sample(sizes, 2)
+  data = {
+    'sizes': random_sizes if bigger_circle == 0 else random_sizes[::-1],
+    'numbers': numbers,
+    'bigger_circle': bigger_circle,
+    'command': command
+  }
+  return jsonify(data)
+
+@app.route("/kolka")
+def page6():
+  return render_template('kolka.html')
+  
+
+@app.route("/final")
+def page7():
+  return render_template('final.html')
   
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
